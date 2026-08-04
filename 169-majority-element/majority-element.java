2678829d6cedd65086c092c27153e1a710c111f9
Sheetal R <sheetalr2605@gmail.com>
2026-08-04 22:02:54 +0530

    Time: 1 ms (99.88%) | Memory: 55.8 MB (18.31%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int num = 0;
+        int count = 0;
+
+        for (int i : nums) {
+            if (count == 0) {
+                num = i;
+            }
+
+            if (i == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return num;
+    }
+}
